90fa888b793521abd7eed7b7fe851452b55525c6
H0onnn <dukei2017@naver.com>
2024-03-17 00:47:29 +0900

    Feat Flex Story

---
+++ services/storybook/stories/ReactComponents/Layout/Flex.stories.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "@h0onnn/react-components-layout/style.css";
+import { Flex as _Flex } from "@h0onnn/react-components-layout";
+
+export default {
+  title: "React Components/Layout/Flex",
+  component: _Flex,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+};
+
+export const FlexStory = {
+  args: {
+    as: "div",
+    padding: "5",
+    background: "pink",
+    boxShadow: "xl",
+    borderRadius: "md",
+    justify: "space-between",
+    style: {
+      width: 300,
+    },
+  },
+  render: (args) => (
+    <_Flex {...args}>
+      <div>테스트</div>
+      <div>입니다</div>
+    </_Flex>
+  ),
+};
