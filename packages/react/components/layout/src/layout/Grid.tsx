5df9794b25c94829d9acb27ca15f904835656e7c
H0onnn <dukei2017@naver.com>
2024-03-17 17:11:59 +0900

    Feat Grid, GridItem Components

---
+++ packages/react/components/layout/src/layout/Grid.tsx
@@ -0,0 +1,61 @@
+import * as React from "react";
+import { GridProps } from "./types";
+import { clsx } from "clsx";
+import { StyleSprinkles } from "../core/style.css";
+import { extractSprinkleProps } from "../utils/properties";
+import { vars } from "@h0onnn/themes";
+
+const Grid = (props: GridProps, ref: React.Ref<HTMLElement>) => {
+  const {
+    as = "div",
+    color,
+    background,
+    children,
+    autoColumns,
+    autoFlow,
+    autoRows,
+    columnGap,
+    column,
+    gap,
+    row,
+    rowGap,
+    templateAreas,
+    templateColumns,
+    templateRows,
+  } = props;
+
+  return React.createElement(
+    as,
+    {
+      ...props,
+      ref,
+      className: clsx([
+        StyleSprinkles(
+          extractSprinkleProps(props, Array.from(StyleSprinkles.properties)),
+        ),
+        props.className,
+      ]),
+      style: {
+        display: "grid",
+        gridAutoColumns: autoColumns,
+        gridAutoFlow: autoFlow,
+        gridAutoRows: autoRows,
+        gridColumn: column,
+        gridColumnGap: columnGap,
+        gridGap: gap,
+        gridRow: row,
+        gridRowGap: rowGap,
+        gridTemplateAreas: templateAreas,
+        gridTemplateColumns: templateColumns,
+        gridTemplateRows: templateRows,
+        color: color && vars.colors.$scale?.[color]?.[700],
+        background: background && vars.colors.$scale?.[background]?.[100],
+        ...props.style,
+      },
+    },
+    children,
+  );
+};
+
+const _Grid = React.forwardRef(Grid);
+export { _Grid as Grid };
